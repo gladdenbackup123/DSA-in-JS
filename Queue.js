2de4275a8d2ddd1167d1c146ee4c8d54036847fe
gladdenbackup123 <122422725+gladdenbackup123@users.noreply.github.com>
2024-02-11 21:17:53 +0530

    Add files via upload

---
+++ Queue.js
@@ -0,0 +1,18 @@
+let queue = [];
+
+//Insertion
+queue.push(1);
+queue.push(2);
+queue.push(3);
+queue.push(4);
+
+console.log(queue);
+
+//Deletion
+queue.shift();
+queue.shift();
+console.log(queue);
+
+//peek -> access front element 
+console.log(queue[0]);
+
